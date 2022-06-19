cd1240d1aa1933d022d9c627a0b26b1623e3e6cb
黄雪朋 <2695009886@qq.com>
2022-06-19 22:34:42 +0800

    测试fetch

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+package main 
 
 import "fmt"
 
@@ -8,4 +8,5 @@ func main() {
 	fmt.Println("这是3")
 	fmt.Println("开始再git stash 的操作开始执行， 并且添加一条数据")
 	fmt.Println("这是git stash 之前的操作")
+	..
 }
